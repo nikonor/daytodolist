b01b9e1d46570a57a1feca71ad6f151f15578705
Leonid Nikolaev <nikonor@nikonor.ru>
2012-03-28 19:38:17 +0400

    first commit

---
+++ st/lang.js
@@ -0,0 +1,24 @@
+var transLang={"ru":{"Lang":"ru",
+					"SkipLink":"Пропустить задачу",
+					"menuSetup":"Настройка",
+					"setupDivLabel":"Настройка",
+					"cancelSetupButton":"Отмена",
+					"Done":"Все сделано, можно идти домой",
+					"saveSetupButton":"Записать"},
+			  "en":{"Lang":"",
+			  		"SkipLink":"Skip task",
+					"menuSetup":"Setup",
+					"setupDivLabel":"Setup",
+					"cancelSetupButton":"Cancel",
+					"Done":"All done, you can go to home",
+					"saveSetupButton":"Save"}
+}				
+
+
+// var en={"Lang":"",
+//		"SkipLink":"",
+// 		"menuSetup":"",
+// 		"setupDivLabel":"",
+// 		"cancelSetupButton":"",
+//		"Done":"",
+// 		"saveSetupButton":""};		
